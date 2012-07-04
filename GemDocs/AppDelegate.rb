c0bb7c2c5f0d4df4526fbc9758bb887b29004b85
Daniel Hedrick <daniel.hedrick@peopleadmin.com>
2012-07-03 21:26:59 -0500

    basic shell of an app.

--- GemDocs/AppDelegate.rb
+++ GemDocs/AppDelegate.rb
@@ -8,8 +8,16 @@
 
 class AppDelegate
     attr_accessor :window
+    attr_accessor :fileTextField
+    
+    def loadGemLockfile(sender)
+        gemlockfile = File.read(fileTextField.stringValue)
+        puts gemlockfile
+    end
+    
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+        puts "applicationDidFinishLaunching!"
     end
 end
 
